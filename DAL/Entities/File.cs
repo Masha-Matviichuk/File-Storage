72ef3b6df98cc0eb0a5f3c0a23d6963f29bd119d
Masha <mariam26022002@gmail.com>
2021-12-20 16:19:06 +0300

    Initial commit

---
+++ DAL/Entities/File.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DAL.Entities
+{
+    public class File : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public DateTime Upload { get; set; }
+        public long Size { get; set; }
+        
+    }
+}
---
+++ DAL/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace DAL.Entities
+{
+    public class User : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+    }
+}
